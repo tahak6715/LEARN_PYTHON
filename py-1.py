d2598d7c8354f31ad18bebbcb0af730c64cd64a0
Taha Khan <99550190+tahak6715@users.noreply.github.com>
2023-09-15 05:14:16 +0500

    day-1

---
+++ py-1.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# 1) Take Input
+name = input("how are you");
+print(name)
+
+
+# In[6]:
+
+
+a = int(input("Enter a number 1"))
+b = int(input("Enter a number 2"))
+
+num = a+b
+print(num);
+
+
+# In[8]:
+
+
+a = 10;
+b = 20;
+sub = a-b
+print(sub)
+
+
+# In[10]:
+
+
+a=56.7;
+b=7.0;
+c = a//b;
+print(c);
+
+
+# In[12]:
+
+
+if True and False:
+    print("hi");
+else:
+    print("hello")
+
+
+# In[13]:
+
+
+if True or False:
+    print("hi");
+else:
+    print("hello")
+
+
+# In[14]:
+
+
+a = True;
+b = False;
+print(not(a));
+
+
+# In[15]:
+
+
+a=10
+b=20
+c=30
+if a!=b:
+    print("Heloo jee")
+
+
+# In[33]:
+
+
+a = 2>4
+b = 3>6
+print(a)
+print(b)
+print(a == b)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
